9e42e5ddd08fbb6d0054331edd2c25a5d6527f0c
AdithyanPadmanabhan <197160@ust.com>
2023-10-27 19:20:50 +0530

    27-10-23

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -302,7 +302,44 @@ Console.WriteLine("Average score of " + student2.studentName + " " + "is" + " " 
 // assignment -3 27-10-23
 
 
-   
+MedicalHistory medicalHistory = new(123, 432, "Cold", "20-10-23");
+
+MedicalHistory medicalHistory1 = new(453, 1232, "Vomiting", "21-01-22");
+
+MedicalHistory medicalHistory2 = new(6783, 332, "Heart Attack", "13-03-21");
+int option, choice;
+do
+{
+    
+    Console.WriteLine("1.Add Medical Details \n 2. View Patient Details\n 3.Exit Order");
+    Console.WriteLine("Choose option");
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+
+            MedicalHistory.MedicalHistoryDetails(medicalHistory1);
+            MedicalHistory.MedicalHistoryDetails(medicalHistory2);
+            MedicalHistory.MedicalHistoryDetails(medicalHistory);
+
+            break;
+
+        case 2:
+
+
+            MedicalHistory.ViewDetails();
+            break;
+
+        case 3:
+            Environment.Exit(0);
+            break;
+    }
+    Console.WriteLine("Do You Want To Continue Press 1 for continue");
+    option = Convert.ToInt32(Console.ReadLine());
+} while (option == 1);
+
+
 
 
 
